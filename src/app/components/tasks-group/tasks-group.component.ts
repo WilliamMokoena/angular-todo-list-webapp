d12805c523babf127cc10bf0191c8b6580bde3fb
Syphon <syphonmechanic@gmail.com>
2021-03-24 10:17:29 +0200

    Finished tablet UI

---
+++ src/app/components/tasks-group/tasks-group.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tasks-group',
+  templateUrl: './tasks-group.component.html',
+  styleUrls: ['./tasks-group.component.scss']
+})
+export class TasksGroupComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
